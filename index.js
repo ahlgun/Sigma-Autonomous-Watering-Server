1b48683dc6b5b9ace78c435d49090ad63918bca3
ahlbomgunnar <Gahlbom@gmail.com>
2017-10-24 14:50:13 +0200

    data update 2

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
 var app = require('express')();
 app.set('port', (process.env.PORT || 5000));
 
-var data = [
-	{
-		name:'Marigold',
-		waterAmountPerDay:'10ml'
-	},
-	{
-		name:'Avocado',
-		waterAmountPerDay:'5ml'
-	},
-]
+
+
+app.get('/', (req, res) => {
+  res.sendFile("index.html", {root: __dirname })
+});
 
 app.get('/ip', (req, res) => {
-  res.send('IP: --- ' + req.ip);
+  res.JSON(req.ip);
 });
 
 app.get('/api', (req, res) => {
-  res.JSON(data);
+  res.JSON(
+	  {
+		name:'Marigold',
+		waterAmountPerDay:'10ml'
+	  },
+	  {
+		name:'Avocado',
+		waterAmountPerDay:'5ml'
+	  }
+	);
 });
+
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
